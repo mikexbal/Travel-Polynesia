dbfc07a1794ed46b646edb727b916b1f5326d68f
Mike <mxbalogun@gmail.com>
2025-02-27 16:08:18 -0500

    Added json functionality and create elements on load using async

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+async function loadDestinations(){
+    try {
+        const response = await fetch("destinationData.json");
+        const destinationData = await response.json();
 
+        let data = "";
+
+        let divContainer = document.getElementById("destination-container");
+        
+        destinationData.islands.forEach(island => { 
+            let div = document.createElement('div');
+            div.className = 'destination-card';
+            div.innerHTML = 
+            `<h3>${island.name}</h3>
+            <p>${island.age}</p>`;
+           divContainer.appendChild(div);
+        });
+
+        document.getElementById("demo").innerHTML = data;
+    } catch (error){
+        console.log("Error fetching JSON:", error)
+    }
+}
+
+window.onload = loadDestinations;
