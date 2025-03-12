6033fa2e5395480dde86f7dfaccde10f3e908335
Mike <mxbalogun@gmail.com>
2025-03-12 14:55:38 -0400

    Fixed issue where location details wouldn't populate with correct ID, need to work on map

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ async function loadDestinations(){
         const response = await fetch("destinationData.json");
         const destinationData = await response.json();
 
-        let data = "";
-
         let divContainer = document.getElementById("destination-container");
         
         destinationData.destinations.forEach(location => { 
             let div = document.createElement('div');
             div.className = 'destination-card';
             div.innerHTML = 
-            `<img src="${location.image}" alt="Hawaii Beach"/>
+            `<img src="${location.image}" alt="${location.name}"/>
             <h2>${location.name}</h2>
             <p>${location.short_desc}</p>`;
+
+            div.addEventListener("click", () => 
+            window.open(`destination.html?id=${location.id}`, "_blank"));
            divContainer.appendChild(div);
         });
 
-        document.getElementById("demo").innerHTML = data;
+     
     } catch (error){
         console.log("Error fetching JSON:", error)
     }
@@ -25,3 +26,25 @@ async function loadDestinations(){
 
 loadDestinations();
 //Create a load booking function for destination.html
+
+async function loadBooking(){
+    try {
+        const response = await fetch("destinationData.json");
+        const destinationData = await response.json();
+
+        const urlParameters = new URLSearchParams(window.location.search);
+        const id = urlParameters.get("id");
+        const location = destinationData.destinations.find(destination => destination.id == id);
+        
+        let destinationInfo = document.getElementById("destination-info");
+        destinationInfo.innerHTML = 
+        `<img src="${location.image}" alt="${location.name}"/>
+        <h1>${location.name}</h1>
+        <p>${location.long_desc}</p>`;
+           
+    } catch (error){
+        console.log("Error fetching JSON:", error)
+    }
+}
+
+loadBooking();
